633fc10f3c21519a15b353a56d0ba405931bea21
Andrade <andrade.nicolas.melo@outlook.com>
2025-05-03 18:16:28 -0300

    Criação do código para ler as dimensões do grid (X = horizontal e Y = vertical) em uma linha, separando e tratando os valores de X e Y individualmente.

--- roboTupiniquinConsoleApp1/DefinidorGrid.cs
+++ roboTupiniquinConsoleApp1/DefinidorGrid.cs
@@ -1,8 +1,28 @@
 ﻿// Responsavel pela definição do tamanho do grid
 
+using System.Threading.Channels;
+
 namespace roboTupiniquinConsoleApp1
 {
-    internal class DefinidorGrid
+    public class DefinidorGrid
     {
+        public int gridEixoX;
+        public int gridEixoY;
+        public void TamanhoGrid()
+        {
+            Console.Write("Tamanho do grid: ");
+            string gridInsercaoValores = "5 5";
+            Console.WriteLine();
+
+            Tratador(gridInsercaoValores);
+        }
+
+        public void Tratador(string gridInsercaoValores)
+        {
+            string[] TamanhoDeCadaEixo = gridInsercaoValores.Split(' ');
+
+            gridEixoX = Convert.ToInt32(TamanhoDeCadaEixo[0]);
+            gridEixoY = Convert.ToInt32(TamanhoDeCadaEixo[1]);
+        }
     }
 }
